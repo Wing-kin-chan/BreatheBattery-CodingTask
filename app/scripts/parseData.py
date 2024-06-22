c730c5ad84a19c00fc6480bdcb7908ee5e661c99
Wing-kin-chan <Wing_kin_chan@hotmail.com>
2024-06-22 22:40:28 +0100

    Updated schema of dictionary yielded by parser to be more consistent with db

--- app/scripts/parseData.py
+++ app/scripts/parseData.py
@@ -1,21 +1,30 @@
+#!/usr/bin/env python
+
 import logging
 from scripts.utils import get_time
 from typing import Generator, Dict, Any
 
-def parseAirData(data: dict) -> Generator[Dict[str, Any], None, None]:
+def parseAirData(data: dict) -> Generator[Dict[str, dict], None, None]:
     """
     A generator function that yields device sensor data:
 
-    - time
-    - SiteName
-    - app
-    - date
-    - device_id
-    - particulates
-    - humidity
-    - temperature
-    - timestamp
-    - app version
+    - device_info:
+        - device_id
+        - SiteName
+        - latitude
+        - longitude
+        - altitude
+        - area
+        - sitename
+        - app_version
+
+    - log_info:
+        - time
+        - date
+        - device_id
+        - particulates
+        - humidity
+        - temperature
 
     Parameters:
 
@@ -42,29 +51,31 @@ def parseAirData(data: dict) -> Generator[Dict[str, Any], None, None]:
         logging.info(f"[{get_time()}]: No number of records found.")
         number_of_records = len(data['feeds'][0][project_name])
 
-    device_record = {}
+    device_record = {'device_info': {'project': project_name},
+                     'log_info': {}}
 
     for idx in range(0, number_of_records):
         key = list(data['feeds'][0][project_name][idx].keys())[0]
         log_data = data['feeds'][0][project_name][idx][key]
 
-        device_record['device_id'] = log_data['device_id']
-        assert device_record['device_id'] == device_id, f"Device ID in record {idx} does not match"
+        assert log_data['device_id'] == device_id, f"Device ID in record {idx} does not match"
+        device_record['device_info']['device_id'] = log_data['device_id']
+        device_record['log_info']['device_id'] = log_data['device_id']
 
         try:
-            device_record['time'] = log_data['time']
+            device_record['log_info']['time'] = log_data['time']
         except KeyError:
             logging.error(f"[{get_time()}] - - - - No time data in record.")
             continue
 
         try:
-            device_record['date'] = log_data['date']
+            device_record['log_info']['date'] = log_data['date']
         except KeyError:
             logging.error(f"[{get_time()}] - - - - No date information in record.")
             continue
         
         try:
-            device_record['particulates'] = float(log_data['s_d0'])
+            device_record['log_info']['particulate2_5'] = float(log_data['s_d0'])
         except KeyError:
             logging.error(f"[{get_time()}] - - - - No particulate data in record.")
             continue
@@ -73,19 +84,19 @@ def parseAirData(data: dict) -> Generator[Dict[str, Any], None, None]:
             continue
 
         try:
-            device_record['sitename'] = log_data['SiteName']
+            device_record['device_info']['sitename'] = log_data['SiteName']
         except KeyError:
             logging.info(f"[{get_time()}] - - - - No sitename in record.")
             pass
 
         try:
-            device_record['area'] = log_data['area']
+            device_record['device_info']['area'] = log_data['area']
         except KeyError:
             logging.info(f"[{get_time()}] - - - - No area name in record.")
             pass
         
         try:
-            device_record['altitude'] = float(log_data['gps_alt'])
+            device_record['device_info']['altitude'] = float(log_data['gps_alt'])
         except KeyError:
             logging.info(f"[{get_time()}] - - - - No altitude data in record.")
             pass
@@ -94,7 +105,7 @@ def parseAirData(data: dict) -> Generator[Dict[str, Any], None, None]:
             pass
 
         try:
-            device_record['latitude'] = float(log_data['gps_lat'])
+            device_record['device_info']['latitude'] = float(log_data['gps_lat'])
         except KeyError:
             logging.info(f"[{get_time()}] - - - - No latitude data in record.")
             pass
@@ -103,7 +114,7 @@ def parseAirData(data: dict) -> Generator[Dict[str, Any], None, None]:
             pass
 
         try:
-            device_record['longitutde'] = float(log_data['gps_lon'])
+            device_record['device_info']['longitutde'] = float(log_data['gps_lon'])
         except KeyError:
             logging.info(f"[{get_time()}] - - - - No longitude data in record.")
             pass
@@ -112,7 +123,7 @@ def parseAirData(data: dict) -> Generator[Dict[str, Any], None, None]:
             pass
 
         try:
-            device_record['temperature'] = float(log_data['s_t0'])
+            device_record['log_info']['temperature'] = float(log_data['s_t0'])
         except KeyError:
             logging.info(f"[{get_time()}] - - - - No temperature data in record.")
             pass
@@ -121,7 +132,7 @@ def parseAirData(data: dict) -> Generator[Dict[str, Any], None, None]:
             pass
 
         try:
-            device_record['humidity'] = float(log_data['s_h0'])
+            device_record['log_info']['humidity'] = float(log_data['s_h0'])
         except KeyError:
             logging.info(f"[{get_time()}] - - - - No humidity data in record.")
             pass
