080c64b9457fd0c2d7334ce4a4b1af3e23c3e62a
Wing-kin-chan <Wing_kin_chan@hotmail.com>
2024-06-24 10:47:13 +0100

    Minor change

--- app/scripts/utils.py
+++ app/scripts/utils.py
@@ -2,4 +2,4 @@ from datetime import datetime
 
 @staticmethod
 def get_time() -> str:
-    return datetime.now().strftime("%d-%m-%Y %H:%M-:%S")
+    return datetime.now().strftime("%d-%m-%Y %H:%M:%S")
