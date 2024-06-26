9f501d48092b50ccd5b96f2b7f89272ffa165b99
Wing-kin-chan <Wing_kin_chan@hotmail.com>
2024-06-26 16:45:34 +0100

    +Routing and logic to download reports

--- app/dashboards/dashboard.py
+++ app/dashboards/dashboard.py
@@ -5,9 +5,20 @@ from scripts.logic import plot, processData
 from scripts.utils import get_time
 from dash import dcc, html
 from datetime import datetime
+from config import Config
+import pandas as pd
 import logging
+import os
 
 logging.basicConfig(level = logging.DEBUG)
+
+REPORTS_DIR = Config.REPORTS_FOLDER_PATH
+
+def save_csv(data: pd.DataFrame, filename: str) -> None:
+    filepath = os.path.join(REPORTS_DIR, filename)
+    logging.info(f"[{get_time()}]: Saving report to {REPORTS_DIR}")
+    data.to_csv(filepath, index = False)
+    return None
 
 def layout(device_id = None):
     try:
@@ -21,6 +32,14 @@ def layout(device_id = None):
     
     daily_stats = plot.dailyStatistics(data)
     line_graph = plot.lineChart(data)
+
+    threshold_file: str = "times_above_threshold.csv"
+    statistics_file: str = "daily_statistics.csv"
+
+    times_above_threshold = processData.getTimesAboveThreshold(data)
+    daily_statistics = processData.getDailyStatistics(data)
+    save_csv(times_above_threshold, threshold_file)
+    save_csv(daily_statistics, statistics_file)
 
     dash_layout = html.Div([
         html.H1(f"{device_id} Historical Data"),
@@ -37,7 +56,16 @@ def layout(device_id = None):
             html.P(f"Time of latest record: {datetime.strftime(metadata.last_updated, '%H:%M:%S %d-%m-%Y')}")
         ]),
         dcc.Graph(figure = daily_stats),
-        dcc.Graph(figure = line_graph)
+        dcc.Graph(figure = line_graph),
+        html.A("Download Times Above Threshold", 
+               href = f'/download/{threshold_file}', 
+               download = threshold_file, 
+               target = "_blank"),
+        html.Br(),
+        html.A("Download Daily Statistics", 
+               href = f'/download/{statistics_file}', 
+               download = statistics_file, 
+               target = "_blank")
     ])
 
     return dash_layout
--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 
-from flask import render_template, Blueprint, request, redirect, url_for, current_app
+from flask import render_template, Blueprint, request, redirect, url_for, current_app, send_from_directory
 from dashboards.dashboard import layout
 from scripts.logic.pipeline import getNewData, checkDeviceExists
 from scripts.utils import get_time
+from config import Config
 import logging
+import os
 
 logging.basicConfig(level = logging.DEBUG)
+DOWNLOAD_FILE_PATH = Config.REPORTS_FOLDER_PATH
 
 pages = Blueprint(name = "pages",
                 import_name = __name__,
@@ -43,3 +46,7 @@ def dashboard(device_id):
     dash_app = current_app.extensions['dash']
     dash_app.layout = layout(device_id)
     return redirect(f"/dash/{device_id}/")
+
+@pages.route('/download/<path:filename>')
+def download_file(filename):
+    return send_from_directory(directory = DOWNLOAD_FILE_PATH, path = filename)
