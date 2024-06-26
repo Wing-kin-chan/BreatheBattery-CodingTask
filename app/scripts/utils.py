3c66cf07ed8bcd99eea97a6062ab6e37b42acf3e
Wing-kin-chan <Wing_kin_chan@hotmail.com>
2024-06-26 16:44:55 +0100

    +Random key generator

--- app/scripts/utils.py
+++ app/scripts/utils.py
@@ -1,5 +1,14 @@
 from datetime import datetime
+import string
+import random
 
 @staticmethod
 def get_time() -> str:
     return datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+
+@staticmethod
+def generate_key() -> str:
+    all_characters = string.ascii_letters + string.digits + string.punctuation
+    # Generate a random string from the set of all characters
+    random_string = ''.join(random.choice(all_characters) for _ in range(50))
+    return str(random_string)
